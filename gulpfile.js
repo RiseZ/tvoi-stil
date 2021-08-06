a2f8c6896d9a0417e921417585754038ef708b46
RiseZ <54645338+RiseZ@users.noreply.github.com>
2021-08-06 08:53:51 +0300

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+gulp.task('styles', () => {
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('watch', () => {
+    gulp.watch('./scss/**/*.scss', gulp.series('styles'));
+});
